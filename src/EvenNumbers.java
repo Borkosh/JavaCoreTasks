287781eb49289cb5e7fe178b7fd087c9d0e9ce00
Borkosh <i.torgoev@gmail.com>
2018-06-19 11:47:50 +0600

    New class EnenNumbers

---
+++ src/EvenNumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Created by Ilgiz on 19.06.2018.
+ */
+public class EvenNumbers {
+    private int a,b;
+    Scanner scan=new Scanner(System.in);
+    public EvenNumbers(){
+        a=scan.nextInt();
+        b=scan.nextInt();
+        getEvenNumbers();
+    }
+
+    public void getEvenNumbers(){
+        for (int i=a; i<=b; i++){
+            if (i%2==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
        // MaxABC calc=new MaxABC();
         //Calc calc=new Calc();
+        EvenNumbers calc=new EvenNumbers();
 
     }
 }
